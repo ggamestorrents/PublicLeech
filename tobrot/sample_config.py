0d01b2175212070cf1062201c670db1df7b90eef
Games Torrents <35608446+ggamestorrents@users.noreply.github.com>
2020-07-29 09:14:41 +0530

    Update sample_config.py

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -55,8 +55,8 @@ class Config(object):
         1072864000
     ))
     # add config vars for the display progress
-    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "█")
-    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "░")
+    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "●")
+    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "◌")
     # add offensive API
     TG_OFFENSIVE_API = get_config("TG_OFFENSIVE_API", None)
     # URL for the rclone configuration
